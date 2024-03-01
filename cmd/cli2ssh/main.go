9ab910b4075e71d5cc6daf95eed77af6d5c19589
peron <peron_ap@icloud.com>
2024-03-01 13:48:03 +0000

    chore: update help message

--- cmd/cli2ssh/main.go
+++ cmd/cli2ssh/main.go
@@ -13,9 +13,9 @@ import (
 
 func main() {
 	var env args.ArrayArg
-	flag.Var(&env, "e", "Set environment variables for the command.")
-	command := flag.String("c", "", "Command to run for each SSH session.")
-	useOsEnv := flag.Bool("os-env", false, "Use the OS environment for the command.")
+	flag.Var(&env, "e", "Set environment variables for each SSH session.")
+	command := flag.String("c", "", "Set the command to run for each SSH session.")
+	useOsEnv := flag.Bool("os-env", false, "Use the OS environment variables for the command.")
 
 	flag.Parse()
 
